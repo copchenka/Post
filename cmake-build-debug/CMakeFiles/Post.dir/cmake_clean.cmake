778c3f567d738616c1072311ee27e1d142211120
copchenka <mandragora2319@mail.ru>
2019-07-22 00:10:44 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Post.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Post.dir/main.c.obj"
+  "CMakeFiles/Post.dir/post.c.obj"
+  "Post.pdb"
+  "Post.exe"
+  "Post.exe.manifest"
+  "libPost.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Post.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
