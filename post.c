a9cd0c1f7a42611488e955b12b75ce2249349f87
copchenka <mandragora2319@mail.ru>
2019-07-22 00:08:23 +0300

    Update post.c

--- post.c
+++ post.c
@@ -11,7 +11,8 @@ tape readTape(char *fileName) {
 	tape.length = 0;
 	tape.head = 0;
 	FILE *tapeFile = fopen(fileName, "r");
-	if (tapeFile == NULL) printf("I can't open this file.\n");
+	if (tapeFile == NULL) 
+		 fprintf(stderr, "I can't open this file.\n");
 	bool isHeadFound = false;
 	int line = 1;
 	char c;
@@ -19,37 +20,46 @@ tape readTape(char *fileName) {
 		switch (c) {
 			case '.':
 				if (!isHeadFound) tape.head++;
-				if (line == 2) printf("This simbol can't be in second line: %c\n", c);
+				if (line == 2) 
+					 fprintf(stderr, "This symbol can't be in second line: %c\n", c);
 				break;
 			case 'V':
-				if (isHeadFound) printf("I find more than one pointer.\n");
-				else if (line == 1) isHeadFound = true;
-				else printf("This simbol can't be in second line: %c\n", c);
+				if (isHeadFound) 
+					 fprintf(stderr, "I find more than one pointer.\n");
+				else if (line == 1) 
+					isHeadFound = true;
+				else  
+					fprintf(stderr, "This symbol can't be in second line: %c\n", c);
 				break;
 			case '0':
 				if (line == 2) {
 					tape.values = (bool *) realloc(tape.values, (tape.length + 1) * sizeof(bool));
-					if (tape.values == NULL) printf("I can't allocate memory for tape.\n");
+					if (tape.values == NULL) 
+						fprintf(stderr, "I can't allocate memory for tape.\n");
 					tape.values[tape.length] = false;
 					tape.length++;
-				} else printf("This simbol can't be in first line: %c\n",c);
+				} else 
+					fprintf(stderr, "This symbol can't be in first line: %c\n", c);
 				break;
 			case '1':
 				if (line == 2) {
 					tape.values = (bool*) realloc(tape.values, (tape.length + 1) * sizeof(bool));
-					if (tape.values == NULL) printf("I can't allocate memory.\n");
+					if (tape.values == NULL)
+						fprintf(stderr, "I can't allocate memory.\n");
 					tape.values[tape.length] = (c == '1');
 					tape.length++;
-				} else printf("This simbol can't be in first line: %c\n",c);
+				} else
+					fprintf(stderr, "This symbol can't be in first line: %c\n", c);
 				break;
 			case '\n':
 				line++;
 				break;
 			default:
-				printf("This simbol can't be use in type: %c\n",c);
+				 fprintf(stderr, "This symbol can't be use in tape: %c\n", c);
 		}
 	}
-	if (!isHeadFound) printf("I can't find pointer.");
+	if (!isHeadFound) 
+		fprintf(stderr, "I can't find pointer.");
 
 	fclose(tapeFile);
 	return tape;
@@ -58,39 +68,49 @@ tape readTape(char *fileName) {
 instr* readInstruction(char *filename) {
     instr *operations = (instr *) malloc(sizeof(instr));
     FILE *instrFile = fopen(filename, "r");
-    if (instrFile == NULL) printf("I can't open this file2.\n");
+    if (instrFile == NULL) 
+	    fprintf(stderr, "I can't open this file2.\n");
     int i = 1;
     int index;
     bool isStopFound = false;
     int f;
 struct instr p;
     while ((f = fscanf(instrFile, "%d. %c", &index, &p.instruction)) != EOF) {
-        if (f < 2) printf("In line must be more than one element.\n");
-        if (index != i) printf("All operations must be numbered in order: 1,2,3,4,...,ect.\n");
+        if (f < 2) 
+		 fprintf(stderr, "In line must be more than one element.\n");
+        if (index != i)
+		fprintf(stderr, "All operations must be numbered in order: 1,2,3,4,...,ect.\n");
         operations = realloc(operations, (i + 1) * sizeof(instr));
-        if (operations == NULL) printf("I can't allocate memory for operations.\n");
+        if (operations == NULL) 
+		 fprintf(stderr, "I can't allocate memory for operations.\n");
         switch (p.instruction) {
 
             case '1':
                 if (fgetc(instrFile) != '\n') {
                     fscanf(instrFile, "%d", &p.next);
-                    if (p.next <= 0) printf("All numbers of operations must be >= 0.\n");
+                    if (p.next <= 0) 
+			     fprintf(stderr, "All numbers of operations must be >= 0.\n");
                 } else p.next = i + 1;
                 break;
             case '?':
+		p.next = 0;
                 if (fscanf(instrFile, "%d; %d", &p.next_if_0, &p.next_if_1) < 2)
-                    printf("Here(%d. operation's) must be more than one element.\n", i);
+                     fprintf(stderr, "Here(%d. operations) must be more than one element.\n", i);
                 if (p.next_if_1 <= 0 || p.next_if_0 <= 0)
-                    printf("All numbers of operations must be >= 0.\n");
+                    fprintf(stderr, "All numbers of operations must be >= 0.\n");
                 break;
             case '!':
+		p.next = 0;
                 isStopFound = true;
                 break;
             case '<':
+		p.next = i + 1;
                 break;
             case '>':
+		p.next = i + 1;
                 break;
             case '0':
+		p.next = i + 1;
                 break;
             default:
                 printf("This symbol(%c, %d) can't be use in operations\n", p.instruction, index);
@@ -99,92 +119,135 @@ struct instr p;
         operations[i] = &p;
         i++;
     }
-    if (!isStopFound) printf("Instructions haven't stop-symbol.\n");
+    if (!isStopFound) 
+	    fprintf(stderr, "Instructions haven't stop-symbol.\n");
 
     fclose(instrFile);
-	return operations;
+    return operations;
 }
-void start(tape taper, instr *operations, int i) {
+void start(char *output,tape taper, instr *operations, int i) {
     //i=0-пошаговый
     //>0 определенное количество шагов
     //-1 просто выводим ответ
-
+    int d;
     int x = 1;
-    if (i == 0 || i == -1) {
-        do {
-            run(taper, operations, x);
-            if (i == 0) {
-                printinstruction(operations, x);
-                printtape(taper);
-            }
-            x++;
-        } while (operations[x]->instruction != '!');
-        if (i == -1) printtape(taper);
+    if (i == 0 || i == -1) d= 50;
+	else d=i;
+    while (x != d + 1 && operations[x].instruction != '!') {
+	    if (operations[x].instruction != '!') {
+            taper = run(taper, operations, x);
 
+            if (i != -1) {
+                printinstruction(output, operations, x);
+                printtape(output, taper);
+            }
+            x = operations[x].next;
+        } else {
+            fprintf(stderr, "The number of steps is out of range.");
+            break;
+        }
+        if (x == d)
+            fprintf(stderr, "Program got stuck.");
     }
-    if (i > 0) {
-        do {
-            if (operations[x]->instruction == '!') {
-                printf("the number of steps more than you can chose. ");/////////////
-                break;
-            } else {
-                run(taper, operations, x);
-                x++;
-            }
 
-        } while (x != i || operations[x]->instruction != '!');
+    if (i == -1) printtape(output, taper);
+    else if (operations[x].instruction == '!') {
+        printinstruction(output, operations, x);
+        printtape(output, taper);
     }
 }
-void run(tape taper, instr *operations, int i) {//выплнение команды под номером i
-    if (operations[i]->instruction == '1') {
-        taper.values[taper.head] = true;
-        taper.head++;
-        i = operations[i]->next;
-    }
-    if (operations[i]->instruction == '0') {
-        taper.values[taper.head] = false;
-    }
-    if (operations[i]->instruction == '>') {//выход за границы
-        taper.head++;
-    }
-    if (operations[i]->instruction == '<') {//выход за границы
-        taper.head--;
-    }
-    if (operations[i]->instruction == '?') {
-        if (taper.values[taper.head] == false) i = operations[i]->next_if_0;
-        else i = operations[i]->next_if_1;
-    }
-    if (operations[i]->instruction == '!') {
-        printtape(taper);
-        printinstruction(operations, i);
-    }
+tape run(tape taper, instr *operations, int i) {//выплнение команды под номером i
+  switch (operations[i].instruction) {
+        case '0':
+            taper.values[taper.head] = false;
+            break;
+        case '>':
+            taper.head++;
+            if (taper.head > taper.length - 1) {
+                taper.values = (bool *) realloc(taper.values, (taper.length + 1) * sizeof(bool));
+                if (taper.values == NULL)
+                    fprintf(stderr, "I can't allocate memory.\n");
+                taper.length++;
+                taper.values[taper.length - 1] = false;
 
+            }
+            break;
+        case '<':
+            taper.head--;
+            if (taper.head < 0) {
+                taper.values = (bool *) realloc(taper.values, (taper.length + 1) * sizeof(bool));
+                if (taper.values == NULL)
+                    fprintf(stderr, "I can't allocate memory.\n");
+                taper.length++;
+                for (int z = taper.length; z > 0; z--) taper.values[z] = taper.values[z - 1];
+                taper.head = 0;
+                taper.values[0] = false;
+            }
+            break;
+        case '?':
+            if (taper.values[taper.head] == false)
+                operations[i].next = operations[i].next_if_0;
+            else
+                operations[i].next = operations[i].next_if_1;
+            break;
+        case '1':
+            taper.values[taper.head] = true;
+            taper.head++;
+            if (taper.head > taper.length - 1) {
+                taper.values = (bool *) realloc(taper.values, (taper.length + 1) * sizeof(bool));
+                if (taper.values == NULL)
+                    fprintf(stderr, "I can't allocate memory.\n");
+                taper.length++;
+                taper.values[taper.length - 1] = false;
+
+            }
+            break;
+        case '!':
+            break;
+        default:
+            break;
+    }
+    return taper;
 }
 
-void printtape(tape result) {
+void printtape(char *file, tape result) {
     FILE *fo;
-    fo = fopen("output.txt", "w");
-    if (fo == NULL) printf("I cant open file for writing");
+    fo = fopen("output.txt", "a");
+    if (fo == NULL) 
+	    fprintf(stderr, "I can't open file for writing.\n");
     int x = 0;
     while (x != result.length) {
-        if (x != result.head)fprintf(fo, ".");
-        else fprintf(fo, "V");
+        if (x != result.head)
+		fprintf(fo, ".");
+        else 
+		fprintf(fo, "V");
         x++;
     }
     fprintf(fo, "\n");
     x = 0;
     while (x != result.length) {
-        if (result.values[x] == false)fprintf(fo, "0");
-        else fprintf(fo, "1");
+        if (result.values[x] == false)
+		fprintf(fo, "0");
+        else 
+		fprintf(fo, "1");
         x++;
     }
+    fprintf(fo, "\n");
+    fclose(fo);
 }
 
-int main() {
-  tape tape1;
-  instr *instruction;
-  tape1=readTape("tape.txt");
-  instruction= readInstruction("instr.txt");
-  printtape(tape1);
-	return 0;
+void printinstruction(char *file, instr *result, int i) {
+    FILE *fo;//
+    fo = fopen(file, "a");
+    if (fo == NULL)
+        fprintf(stderr, "I cant open file for writing.\n");
+    fprintf(fo, "%d. ", i);
+    if (result[i].instruction == '1')
+        fprintf(fo, "%c %d\n", result[i].instruction, result[i].next);
+    else if (result[i].instruction == '?')
+        fprintf(fo, "%c %d; %d\n", result[i].instruction, result[i].next_if_0, result[i].next_if_1);
+    else
+        fprintf(fo, "%c\n", result[i].instruction);
+    fclose(fo);
 }
+
